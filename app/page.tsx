c560eb4ae8c98ef3cae02d8a6fc8f9323de8e1c2
Kevin <vincitbussiness@gmail.com>
2024-08-18 03:54:54 -0600

    Setting up color

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default async function Index() {
   const isSupabaseConnected = canInitSupabaseClient();
 
   return (
-    <div className="flex-1 w-full flex flex-col gap-20 items-center bg-green">
+    <div className="flex-1 w-full flex flex-col gap-20 items-center bg-red-1">
       <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
         <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
         </div>
