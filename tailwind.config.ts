8280f7732ceaad06cf7e735c56b2092a962d346f
Kevin Perdomo <pg1807@ues.edu.sv>
2024-08-18 02:59:05 -0600

    Setting up

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,11 +9,21 @@ module.exports = {
       colors: {
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
+        'white': '#ffffff',
+        'black': '#292F36',
+        'red': "FF6B6B",
+        'yellow': "FFE66D",
+        'green': "4ECDC4",
+        'white-soft': "F7FFF7",
         btn: {
           background: "hsl(var(--btn-background))",
           "background-hover": "hsl(var(--btn-background-hover))",
         },
       },
+      fontFamily : {
+        'tilt': ['Tilt Warp', 'sans-serif'],
+        'league': ['League Spartan', 'sans-serif'],
+      }
     },
   },
   plugins: [],
