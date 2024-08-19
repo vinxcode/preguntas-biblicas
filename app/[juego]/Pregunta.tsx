d5c107eb345b6fb17f2179809b1d81a71cd70c57
Kevin <vincitbussiness@gmail.com>
2024-08-19 13:38:52 -0600

    Modal logical has been created with the useSotre

--- app/[juego]/Pregunta.tsx
+++ app/[juego]/Pregunta.tsx
@@ -1,18 +1,22 @@
 import React from 'react'
+import { useStore } from '@/app/store/useStore'
 
 type PreguntaProps = {
     id_pregunta: number
 }
 
 export default function Pregunta(props: PreguntaProps) {
-const handleSelect = (idPregunta: number) => {
 
-}
+    const isPreguntaModalOpen = useStore((state) => state.isPreguntaModalOpen)
+    const updateIsPreguntaModalOpen = useStore((state) => state.updateIsPreguntaModalOpen)
+
+    const handleSelect = (idPregunta: number) => {
+        updateIsPreguntaModalOpen(true)
+    }
 
     return (
         <article className="bg-red-3 flex justify-center items-center font-tilt py-10 rounded-xl shadow-lg border-4 border-blue hover:bg-red-2 cursor-pointer"
-        onClick={() => handleSelect(props.id_pregunta)}>
-            
+            onClick={() => handleSelect(props.id_pregunta)}>
             <p className="text-white text-3xl">{props.id_pregunta}</p>
         </article>
     )
--- app/[juego]/page.tsx
+++ app/[juego]/page.tsx
@@ -4,6 +4,7 @@ import { useParams } from "next/navigation"
 import { createClient } from "@/utils/supabase/client"
 import { useState, useEffect } from "react"
 import Pregunta from "./Pregunta"
+import { useStore } from '@/app/store/useStore'
 
 type Deck = {
     id_deck: number,
@@ -27,6 +28,8 @@ export default function Juego() {
     const supabase = createClient()
     const { juego } = useParams()
     const [preguntas, setPreguntas] = useState<Pregunta[]>([])
+    const isPreguntaModalOpen = useStore((state) => state.isPreguntaModalOpen)
+    const updateIsPreguntaModalOpen = useStore((state) => state.updateIsPreguntaModalOpen)
 
     useEffect(() => {
         const getPreguntas = async () => {
@@ -45,8 +48,8 @@ export default function Juego() {
                     nombre_dificultad
                 )
             `)
-            .eq('deck', juego)
-            
+                .eq('deck', juego)
+
             if (error) {
                 console.error('Error fetching preguntas:', error);
             } else if (data) {
@@ -59,19 +62,28 @@ export default function Juego() {
         getPreguntas()
     }, [supabase])
 
-    const handleSelect = (idPregunta: number) => {
-
-    }
-
     return (
-        <div className="grid grid-cols-8 w-full gap-2 mt-10">
-            {
-                preguntas.map(pregunta => (
-                        <Pregunta key={pregunta.id_pregunta} 
-                        id_pregunta={pregunta.id_pregunta}
+        <div className="w-full h-screen border-2  flex flex-col items-center">
+            
+            <div className="grid grid-cols-8 w-full gap-2 mt-10">
+                {
+                    preguntas.map(pregunta => (
+                        <Pregunta key={pregunta.id_pregunta}
+                            id_pregunta={pregunta.id_pregunta}
                         />
-                ))
-            }
+                    ))
+                }
+            </div>
+
+        {
+            isPreguntaModalOpen && (
+                <div className="absolute bg-white-bg shadow-lg p-4  flex flex-col z-10 rounded-lg w-96 mx-auto">
+                    <p className="text-black">MODAL</p>
+                    <button onClick={() => updateIsPreguntaModalOpen(false)}
+                    className="bg-red-1 py-3 rounded-lg w-full text-white">Close modal</button>
+                </div>
+            )
+        }
         </div>
 
     )
