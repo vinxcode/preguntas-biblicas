d84e217c2228bf3e7c8c29ef3465a071ce5a63c7
Kevin <vincitbussiness@gmail.com>
2024-08-25 16:04:00 -0600

    Imporving the view and adding some validations before to do the POST request

--- app/create-deck/page.tsx
+++ app/create-deck/page.tsx
@@ -17,49 +17,53 @@ export default function CreateDeck() {
     const handleSubmit = async (e: any) => {
         e.preventDefault();
 
-        try {
-            const response = await fetch('/api/mazo', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    nombre_deck: nombreDeck,
-                    descripcion_deck: descripcionDeck,
-                    numero_preguntas: numeroPreguntas
-                })
-            });
+        if (nombreDeck !== "" && descripcionDeck !== "") {
+            try {
+                const response = await fetch('/api/mazo', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        nombre_deck: nombreDeck,
+                        descripcion_deck: descripcionDeck,
+                        numero_preguntas: numeroPreguntas
+                    })
+                });
 
-            if (!response.ok) {
-                throw new Error('Error en la solicitud');
+                if (!response.ok) {
+                    throw new Error('Error en la solicitud');
+                }
+
+                setNombreDeck("")
+                setDescripcionDeck("")
+                setNumeroPreguntas(10)
+
+                const data = await response.json();
+            } catch (error) {
+                console.error('Error al insertar los datos:', error);
             }
-
-            setNombreDeck("")
-            setDescripcionDeck("")
-            setNumeroPreguntas(10)
-
-            const data = await response.json();
-        } catch (error) {
-            console.error('Error al insertar los datos:', error);
+        } else {
+            alert('onde vaaaas prrro')
         }
 
     };
     return (
-        <section className=' bg-white-bg w-full mt-0 h-screen rounded-b-xl p-10 font-league'>
+        <section className='bg-white-bg w-full my-auto rounded-xl p-20 font-league text-blue'>
             <h1 className='text-center text-3xl font-bold'>Crear un nuevo mazo</h1>
 
-            <div className='flex flex-col items-center gap-3 p-5 w-[400px] mx-auto rounded-lg border-4 border-blue mt-5'>
+            <div className='flex flex-col items-center gap-3 p-5 w-3/4 mx-auto rounded-lg border-4 border-blue mt-5'>
                 <div className='flex flex-col w-full'>
                     <label htmlFor="mazo">Nombre del mazo</label>
                     <input type="text" name="mazo" id="mazo" value={nombreDeck} placeholder='Ej. Profetas de la biblia'
-                        className='py-2 px-5 rounded-lg border-4 border-blue' 
-                        onChange={(e)=> setNombreDeck(e.target.value)}/>
+                        className='py-2 px-5 rounded-lg border-4 border-blue'
+                        onChange={(e) => setNombreDeck(e.target.value)} />
                 </div>
                 <div className='flex flex-col w-full'>
                     <label htmlFor="descripcion">Descripcion</label>
                     <input type="text" name="descripcion" id="descripcion" value={descripcionDeck} placeholder='Ej. Acerca de los profetas menores de la biblia'
-                        className='py-2 px-5 rounded-lg border-4 border-blue' 
-                        onChange={(e)=> setDescripcionDeck(e.target.value)}/>
+                        className='py-2 px-5 rounded-lg border-4 border-blue'
+                        onChange={(e) => setDescripcionDeck(e.target.value)} />
                 </div>
                 <div className='flex flex-col w-full'>
                     <label>Numero de preguntas</label>
@@ -72,7 +76,7 @@ export default function CreateDeck() {
                     </div>
                 </div>
                 <button className='w-full bg-red-2 hover:bg-red-3 text-white py-3 rounded-lg font-semibold'
-                onClick={handleSubmit}>Crear mazo</button>
+                    onClick={handleSubmit}>Crear mazo</button>
             </div>
 
         </section>
