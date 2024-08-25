0b5150939c98445e79413580dc9820e043f76a8a
Kevin <vincitbussiness@gmail.com>
2024-08-25 11:47:19 -0600

    Creating route for insert decks

---
+++ app/api/mazo/route.ts
@@ -0,0 +1,24 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl: any = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey: any = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+
+export async function POST(request: any) {
+    const { nombre_deck, descripcion_deck, numero_preguntas } = await request.json();
+
+    try {
+        const { data, error } = await supabase
+            .from('decks')
+            .insert([{ nombre_deck, descripcion_deck, numero_preguntas }]);
+
+        if (error) {
+            throw error;
+        }
+
+        return new Response(JSON.stringify({ data }), { status: 200 });
+    } catch (e: any) {
+        return new Response(JSON.stringify({ error: e.message }), { status: 500 });
+    }
+}
