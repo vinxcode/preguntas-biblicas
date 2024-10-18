d53dde40473d8ed3c577e17b24acb96948e7019d
Kevin <vincitbussiness@gmail.com>
2024-10-17 19:12:26 -0600

    Responsiveness in HIW

--- app/ComoFunciona.tsx
+++ app/ComoFunciona.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function ComoFunciona() {
     return (
-        <div className='flex flex-col gap-5 w-3/4 mx-auto mt-20 bg-white text-blue p-10 rounded-xl shadow-lg'>
+        <div className='flex flex-col gap-5 w-full md:w-3/4 mx-auto mt-20 bg-white text-blue p-7 rounded-xl shadow-lg'>
             <h2 className='text-3xl font-black'>Como funciona Juego Biblico</h2>
             <div className='bg-red-1 rounded-full w-full h-2'></div>
             <article className='flex flex-col gap-2'>
