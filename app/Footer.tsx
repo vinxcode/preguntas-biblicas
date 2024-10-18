974a48ec8b3046bdcb59bbe4ca4ae06f92a2c740
Kevin <vincitbussiness@gmail.com>
2024-10-17 19:02:24 -0600

    Fixing footer

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Footer() {
     return (
-        <div className='text-white w-full flex flex-col items-center font-league pt-28 mt-20 bottom-0'>
-            <p>Hecho con amor por <a href="https://vinxcode.github.io">@vinxcodejs</a></p>
+        <div className='text-white w-full flex flex-col items-center font-league py-14 mt-20 bottom-0'>
+            <p>Made with love by <a href="https://vinxcode.github.io" >@vinxcodejs</a></p>
         </div>
     )
 }
